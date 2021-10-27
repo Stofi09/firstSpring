5b7c498ca2c8038d03b82737fa238c9e3bc523b0
Kris <kristof.solak@gmail.com>
2021-10-27 17:41:10 +0200

    Thanos snapper added

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 @RestController
 public class Controller {
@@ -31,6 +33,27 @@ public class Controller {
         cats.add(cat4);
 
         return  gson.toJson(cats);
+    }
+    @GetMapping("/getrandomcat")
+    public String getRandomCat(){
+        Cat cat = new Cat("cat1", "breed");
+        Cat cat2 = new Cat("cat2", "breed");
+        Cat cat3 = new Cat("cat3", "breed");
+        Cat cat4 = new Cat("cat4", "breed");
+
+        ArrayList<Cat> cats = new ArrayList<>();
+        cats.add(cat);
+        cats.add(cat2);
+        cats.add(cat3);
+        cats.add(cat4);
+        Collections.shuffle(cats);
+        return  gson.toJson(cats.get(0));
+    }
+
+    @GetMapping("/hastanoserasedme")
+    public String thanos(){
+        double num = Math.random()*10;
+        return num > 5 ? "Yes you are dead" : "Nah you are fine";
     }
 
     @GetMapping("/setname")
