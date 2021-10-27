f47345e28af908df0cb60a94420bbe3f2798e03d
Kris <kristof.solak@gmail.com>
2021-10-27 16:59:47 +0200

    Cat added

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.nums.Cat;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private Gson gson;
+
 
     @GetMapping("/")
     public String index(){
-        return "index";
+        Cat cat = new Cat("name4", "brdasdeed");
+        return  gson.toJson(cat);
     }
 }
---
+++ src/main/java/com/example/demo/nums/Cat.java
@@ -0,0 +1,13 @@
+package com.example.demo.nums;
+
+public class Cat {
+
+    private String name;
+    private String breed;
+
+    public Cat(String name,String breed){
+        this.name = name;
+        this.breed = breed;
+    }
+
+}
